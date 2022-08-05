cf3b2a26d5456d6a1dde3ed3607c9b90c8ef27bd
Pavan Sastry NVSS <pavan3457.us@gmail.com>
2022-08-05 12:31:23 +0530

    august_5_2022

---
+++ valid paranthesis.py
@@ -0,0 +1,11 @@
+def isValid( s: str) -> bool:
+    stack = []
+    dic = {'(': ')', '[': ']', '{': '}'}
+    for c in s:
+        if stack and stack[-1] in "([{" and dic[stack[-1]] == c:
+            stack.pop()
+        else:
+            stack.append(c)
+    return not stack
+
+print(isValid('[()]'))
